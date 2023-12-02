7c4930f2d10cc7bc6cc137a14c623934ecef6e46
Quentin Verdier <quent1.verdier@gmail.com>
2023-12-02 17:22:00 +0100

    🔧 Refactor code structure and update favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -8,7 +8,6 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
       </Head>
       <Curtains>
         <Component {...pageProps} />
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div className="text-center">
+    <main className="text-center">
       <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">Bienvenue dans le cirque</h1>
-    </div>
+    </main>
   );
 }
