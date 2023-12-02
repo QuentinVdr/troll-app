4c4a48314827c90665f65fe8bdb6d2dea6534a87
Quentin Verdier <quent1.verdier@gmail.com>
2023-12-02 15:28:31 +0100

    💄 Update Curtain style

--- src/pages/test.js
+++ src/pages/test.js
@@ -3,35 +3,37 @@ import styles from '../styles/test.module.css';
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.content}></div>
+      <div className={styles.content}>
+        <h1 className={styles.title}>Content</h1>
+      </div>
       <div className={styles.curtainBody}>
-        <div id="leftCurtain" className={styles.curtainContainer}>
-          <div className={styles.unCurtain}></div>
-          <div className={styles.unCurtain}></div>
-          <div className={styles.unCurtain}></div>
-          <div className={styles.unCurtain}></div>
-          <div className={styles.unCurtain}></div>
-          <div className={styles.unCurtain}></div>
-          <div className={styles.unCurtain}></div>
-          <div className={styles.unCurtain}></div>
-          <div className={styles.unCurtain}></div>
-          <div className={styles.unCurtain}></div>
-          <div className={styles.unCurtain}></div>
-          <div className={styles.unCurtain}></div>
+        <div id={styles.leftCurtains} className={styles.curtainContainer}>
+          <div className={styles.curtain}></div>
+          <div className={styles.curtain}></div>
+          <div className={styles.curtain}></div>
+          <div className={styles.curtain}></div>
+          <div className={styles.curtain}></div>
+          <div className={styles.curtain}></div>
+          <div className={styles.curtain}></div>
+          <div className={styles.curtain}></div>
+          <div className={styles.curtain}></div>
+          <div className={styles.curtain}></div>
+          <div className={styles.curtain}></div>
+          <div className={styles.curtain}></div>
         </div>
-        <div id="rightCurtain" className={styles.curtainContainer}>
-          <div className={styles.unCurtain}></div>
-          <div className={styles.unCurtain}></div>
-          <div className={styles.unCurtain}></div>
-          <div className={styles.unCurtain}></div>
-          <div className={styles.unCurtain}></div>
-          <div className={styles.unCurtain}></div>
-          <div className={styles.unCurtain}></div>
-          <div className={styles.unCurtain}></div>
-          <div className={styles.unCurtain}></div>
-          <div className={styles.unCurtain}></div>
-          <div className={styles.unCurtain}></div>
-          <div className={styles.unCurtain}></div>
+        <div id={styles.rightCurtains} className={styles.curtainContainer}>
+          <div className={styles.curtain}></div>
+          <div className={styles.curtain}></div>
+          <div className={styles.curtain}></div>
+          <div className={styles.curtain}></div>
+          <div className={styles.curtain}></div>
+          <div className={styles.curtain}></div>
+          <div className={styles.curtain}></div>
+          <div className={styles.curtain}></div>
+          <div className={styles.curtain}></div>
+          <div className={styles.curtain}></div>
+          <div className={styles.curtain}></div>
+          <div className={styles.curtain}></div>
         </div>
         <div className={styles.overlay}></div>
       </div>
